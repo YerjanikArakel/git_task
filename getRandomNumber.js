0815ad193f75a7ee871e1401e0467cdcb1796b29
Yerjanik Arakel <arakelyanyerjanik@gmail.com>
2020-06-28 14:42:02 +0400

    Contents are changed in right places

--- getRandomNumber.js
+++ getRandomNumber.js
@@ -1,17 +1,9 @@
-const getRandomletter = (length) => {
-  let randomChars =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  let result = "";
-  for (let i = 0; i < length; i++) {
-    result += randomChars.charAt(
-      Math.floor(Math.random() * randomChars.length)
-    );
-  }
-  return result;
+const getRandomNumber = () => {
+  return Math.random();
 };
 
-console.log(getRandomletter(1));
-console.log(getRandomletter(1));
-console.log(getRandomletter(1));
-console.log(getRandomletter(1));
-console.log(getRandomletter(1));
+console.log(getRandomNumber());
+console.log(getRandomNumber());
+console.log(getRandomNumber());
+console.log(getRandomNumber());
+console.log(getRandomNumber());
