af83107e042bb693563e8d193a0d893fdca33756
Yerjanik Arakel <arakelyanyerjanik@gmail.com>
2020-06-28 14:35:46 +0400

    Files contents are swaped

--- getRandomNumber.js
+++ getRandomNumber.js
@@ -1,9 +1,17 @@
-const getRandomNumber = () => {
-  return Math.random();
+const getRandomletter = (length) => {
+  let randomChars =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += randomChars.charAt(
+      Math.floor(Math.random() * randomChars.length)
+    );
+  }
+  return result;
 };
 
-console.log(getRandomNumber());
-console.log(getRandomNumber());
-console.log(getRandomNumber());
-console.log(getRandomNumber());
-console.log(getRandomNumber());
+console.log(getRandomletter(1));
+console.log(getRandomletter(1));
+console.log(getRandomletter(1));
+console.log(getRandomletter(1));
+console.log(getRandomletter(1));
