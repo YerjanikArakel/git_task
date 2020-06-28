ac4be9c5da037a5fac50fde550b814d34be18ff1
Yerjanik Arakel <arakelyanyerjanik@gmail.com>
2020-06-28 14:22:16 +0400

    getRandomNumber.js is changed

---
+++ getRandomNumber.js
@@ -0,0 +1,9 @@
+const getRandomNumber = () => {
+  return Math.random();
+};
+
+console.log(getRandomNumber());
+console.log(getRandomNumber());
+console.log(getRandomNumber());
+console.log(getRandomNumber());
+console.log(getRandomNumber());
